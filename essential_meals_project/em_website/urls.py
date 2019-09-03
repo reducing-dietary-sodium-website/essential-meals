08b263861337182a9ee30eefbd2b44a2fc131a51
JoonyeolOh <zbqmal@gmail.com>
2019-09-03 18:16:49 -0400

    Set up Login and Home connection

--- essential_meals_project/em_website/urls.py
+++ essential_meals_project/em_website/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('hello', views.hello, name='helloworld'),
+    path('login', views.login, name='em_login'),
+    path('home', views.home, name='em_home'),
 ]
--- essential_meals_project/em_website/views.py
+++ essential_meals_project/em_website/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+	return HttpResponse("Hello, World. You're at the em_website's index.")
+
 def hello(request):
 	return render(request, "helloworld.html")
+
+def login(request):
+	return render(request, "login.html", {'title': 'Login'})
+
+def home(request):
+	return render(request, "home.html", {'title': 'Home'})
