eb4909b4795a676ad1ad3a511597421b8af6f138
Billy Vaughn <vaughnbilly8@gmail.com>
2019-09-03 17:52:34 -0400

    added template paths to be able to add html pages

--- essential_meals_project/em_website/views.py
+++ essential_meals_project/em_website/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return HttpResponse("Hello, World. You're at the em_website's index.")
+
+def hello(request):
+	return render(request, "helloworld.html")
