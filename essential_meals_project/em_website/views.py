d026f2c9151a61b314f3022aa1e1ad79b963d844
JoonyeolOh <zbqmal@gmail.com>
2019-09-03 17:53:53 -0400

    nothing

--- essential_meals_project/em_website/views.py
+++ essential_meals_project/em_website/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return HttpResponse("Hello, World. You're at the em_website's index.")
+# def login(request):
+# 	return render(request, 'login.html')
--- essential_meals_project/essential_meals_project/settings.py
+++ essential_meals_project/essential_meals_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'em_website.apps.EmWebsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
