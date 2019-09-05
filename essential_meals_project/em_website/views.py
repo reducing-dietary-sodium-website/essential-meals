b8c9d5195ae636163b1eeeff209350a1e3a74549
loimac <loimac111@gmail.com>
2019-09-05 15:59:05 -0400

    Home page, messed up other commits

--- essential_meals_project/em_website/views.py
+++ essential_meals_project/em_website/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, redirect
+
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import NewTopicForm
+from .models import Board, Topic, Post
 
 # Create your views here.
 def index(request):
@@ -12,4 +16,29 @@ def login(request):
 	return render(request, "Registration/login.html", {'title': 'Login'})
 
 def home(request):
-	return render(request, "home.html", {'title': 'Home'})
+	boards = Board.objects.all()
+	return render(request, "home.html", {'boards': boards})
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = request.user  # <- here
+            topic.save()
+            Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=request.user  # <- and here
+            )
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
+def board_topics(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request, 'topics.html', {'board': board})
+def topic_posts(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    return render(request, 'topic_posts.html', {'topic': topic})
--- essential_meals_project/essential_meals_project/settings.py
+++ essential_meals_project/essential_meals_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 LOGIN_REDIRECT_URL = '/em_website/home/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
