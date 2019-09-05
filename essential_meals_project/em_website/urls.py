e5e456c9ec6c20cca3eaee75922b94089c5a836a
dkey9 <dkey9@gatech.edu>
2019-09-05 18:40:22 -0400

    Fixed missing bracket

--- essential_meals_project/em_website/urls.py
+++ essential_meals_project/em_website/urls.py
@@ -13,3 +13,4 @@ urlpatterns = [
     re_path('boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path('boards/(?P<pk>\d+)/new$', views.new_topic, name='new_topic'),
     re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.topic_posts, name='topic_posts'),
+]
